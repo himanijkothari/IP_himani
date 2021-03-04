203fb5bb49a766e70ae3e14046d11d2aa84c2c58
Himani Paronigar <himanijkothari@gmail.com>
2021-03-04 23:37:29 +0100

    separate class for task, tasklist and write task to text

--- ToDoList/src/Main.java
+++ ToDoList/src/Main.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+            System.out.println("Welcome to ToDoLy");
+            System.out.println("You have X tasks todo and Y tasks are done");
+            System.out.println("Pick an option:");
+            System.out.println("(1)Show Task List(by date or project");
+            System.out.println("(2)Add New Task");
+            System.out.println("(3)Edit Task(update, mark as done, remove)");
+            System.out.println("(4)Save and quit");
+
+            Scanner sc = new Scanner(System.in);
+            int choice = sc.nextInt();
+
+            switch(choice)
+            {
+                case 1:
+
+
+            }
+        }
     }
-}
+
