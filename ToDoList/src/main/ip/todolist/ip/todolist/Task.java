5b41a3f15eab37f5558fa25093b87466a3829e70
Himani Paronigar <himanijkothari@gmail.com>
2021-03-19 12:44:56 +0100

    solved some issue related to input and empty tasklist

--- ToDoList/src/main/ip/todolist/ip/todolist/Task.java
+++ ToDoList/src/main/ip/todolist/ip/todolist/Task.java
@@ -1,96 +1,119 @@
 /**
- * The Task class is used to create an individual task
+ * This is a model class and it represents a Task object
+ * and it contains necessary fields and methods to operate
+ * on task object
  * @author: Himani Paronigar
+ * @version : 1.0
+ * @since : 2021-03-19
  */
 package  ip.todolist;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.io.Serializable;
-public class Task implements Serializable{
+public class Task implements Serializable {
 
-
-        public static int count = 0;
-        private int taskID;
         private String title;    // Represents task title.
         private boolean status;  // Represents task status.
         private String projectName;  // Represents project name associate with task.
         private LocalDate dueDate;    // Represents task due date.
 
+
+        /**
+         * Creating an object of Task class
+         * @param title A String that holds the title of a task
+         * @param projectName A String that holds the name of project associated with task
+         * @param dueDate The due date of the task
+         */
         public Task(String title, LocalDate dueDate, String projectName) {
-            setTaskID();
+
             setTitle(title);
             setProjectName(projectName);
             setDueDate(dueDate);
             this.status = false;    // Setting by default status of task as false. False indicates pending task.
 
         }
-        public void setTaskID()
-        {
-            this.taskID = ++count;
-        }
 
-        public int getTaskID()
-        {
-            return taskID;
-        }
 
-        public void setTitle(String title)
-        {
+        /**
+         * A method to set the title of a Task object
+         * @param title A String that holds the title of a task and it cannot be empty or null.
+         * @throws NullPointerException if title is null or empty string
+         */
+        public void setTitle(String title) throws NullPointerException {
             if(title == null)
                 throw new NullPointerException("Title can not be empty");
             this.title = title.trim();
         }
 
+        /**
+         * A method to get the task title
+         * @return a String containing the title of a task
+         */
         public String getTitle()
         {
             return title;
         }
 
-        public void setProjectName(String projectName)
-        {
+        /**
+         * A method to set the project name
+         * @param projectName A String that holds the name of project associated with task
+         * @throws NullPointerException if title is null or empty string
+         */
+        public void setProjectName(String projectName) throws NullPointerException {
             if(projectName == null)
                 throw new NullPointerException("Project can not be empty");
             this.projectName = projectName;
         }
 
 
+        /**
+         * A method to get the project name
+         * @return a String containing the project name
+         */
         public String getProjectName()
         {
             return projectName;
         }
 
-        public void setStatus(boolean status)
-        {
-            this.status = status;
-        }
-
+        /**
+         * A method to get the project name
+         * @return a Boolean containing status of the task
+         */
         public boolean getStatus()
         {
             return status;
         }
 
-        public boolean markComplete()
-        {
-            this.status = true;
-            return this.status;
-        }
 
-        public void setDueDate(LocalDate dueDate)
+        /**
+         * A method to set the due date of a task
+         * @param dueDate The due date of the task
+         * @throws DateTimeException if given date is a past date
+         */
+        public void setDueDate(LocalDate dueDate) throws DateTimeException
         {
             if(dueDate.compareTo(LocalDate.now()) < 0)
-            {
-               throw new DateTimeException("Enter date after today");
-            }
-            this.dueDate = dueDate;
+                throw new DateTimeException("Enter date after today");
+
+            this.dueDate = LocalDate.parse(dueDate.format(DateTimeFormatter.ISO_LOCAL_DATE));
         }
 
+
+        /**
+         * A method to get the due date of the task
+         * @return the due date of task as LocalDate object
+         */
         public LocalDate getDueDate()
         {
             return dueDate;
         }
 
+        /**
+         * A method to mark a task as completed
+         */
         public void markAsDone()
         {
             this.status = true;
@@ -99,6 +122,10 @@ public class Task implements Serializable{
 }
 
 
+/**
+ * This class implements the comparator interface
+ * that helps to sort the tasklist by date
+ */
 class SortbyDate implements Comparator<Task>
 {
     @Override
@@ -108,6 +135,10 @@ class SortbyDate implements Comparator<Task>
     }
 }
 
+/**
+ * This class implements the comparator interface
+ * that helps to sort the tasklist by projectName
+ */
 class SortbyProjectName implements Comparator<Task>
 {
     @Override
--- ToDoList/src/main/ip/todolist/ip/todolist/Task_List.java
+++ ToDoList/src/main/ip/todolist/ip/todolist/Task_List.java
@@ -1,6 +1,8 @@
-/** Represents List of available task.
+/** This class represents ToDoList which contains the ArrayList of Task objects
  * @author : Himani Paronigar
- */
+ * @version : 1.0
+ *@since : 2021-03-19
+ **/
 
 package ip.todolist;
 
@@ -17,54 +19,106 @@ import java.time.format.DateTimeFormatter;
 import  java.util.ArrayList;
 
 public class Task_List {
-
+    
+    // An array list of task objects
     private ArrayList<Task> taskList;
 
+    //Creating todolist object
     public Task_List()
     {
         taskList = new ArrayList<Task>();
     }
 
+    /**
+     * Adding a Task object in ArrayList
+     * @param task that is already created
+     */
     public void addTaskList(Task task)
     {
         taskList.add(task);
     }
 
+
+    /**+
+     * Check whether TaskList is empty
+     * @return true if tasklist is empty , false otherwise
+     */
+    public boolean isTaskListEmpty()
+    {
+      if(taskList.size() == 0)
+          return true;
+      else
+          return false;
+    }
+
+
+    /**
+     * A method to read the value from user (standard input, i.e., terminal)
+     * to create a Task object and to add in the ArrayList of Tasks
+     */
+    public void readTaskFromUser()
+    {
+        try
+        {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the title of task:");
+            String title = Main.getStrInputFromUser();  //Calling this method for restricting user to enter string only
+            System.out.print("Enter the project associated with task:");
+            String projectName = Main.getStrInputFromUser();  //Calling this method for restricting user to enter string only
+            System.out.print("Enter the due date of task(for example:2019-12-02):");
+            String date = sc.next();
+            LocalDate dueDate = LocalDate.parse(date, DateTimeFormatter.ISO_LOCAL_DATE);
+            Task new_task = new Task(title, dueDate, projectName);
+            addTaskList(new_task);           //adding newly created task to tasklist object
+            System.out.println("Task added Successfully");
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * A method to display the contents of ArrayList with first column as task ID
+     */
     public void displayAllTask()
     {
-        String displayFormat="%-4s%-35s%-20s%-20s%-10s";
-        System.out.println(String.format(displayFormat,"ID","TITLE","PROJECT-NAME","DUEDATE","STATUS"));
-        System.out.println(String.format(displayFormat,"==","=====","============","========","======"));
-        for(Task task:taskList){
+        String displayFormat = "%-4s%-35s%-20s%-20s%-10s";
+        System.out.println(String.format(displayFormat, "ID", "TITLE", "PROJECT-NAME", "DUEDATE", "STATUS"));
+        System.out.println(String.format(displayFormat, "==", "=====", "============", "========", "======"));
+        for (Task task : taskList) {
 
-            System.out.println(String.format(displayFormat,task.getTaskID(),task.getTitle(),task.getProjectName(),task.getDueDate(),(task.getStatus()?"COMPLETED":"NOT COMPLETED")));
+            System.out.println(String.format(displayFormat, taskList.indexOf(task) + 1, task.getTitle(), task.getProjectName(), task.getDueDate(), (task.getStatus() ? "COMPLETED" : "NOT COMPLETED")));
         }
-
     }
 
+     /**
+     * A method to display the contents of ArrayList
+     * @param sortBy a string holding a number, 1 for sorting by date and 2 for sorting by date
+     */
     public void listAllTaskFromList(int sortBy)
     {
-        if(taskList.size()==0)
-            System.out.println("No task in list");
-        else
-        if(sortBy==1)//by date
+        if(sortBy == 1)
         {
-            taskList.sort(new SortbyDate());
+            taskList.sort(new SortbyDate());            //Sorting tasks by Date
             displayAllTask();
         }
-        else
+        else if(sortBy == 2)
         {
-            taskList.sort(new SortbyProjectName());
+            taskList.sort(new SortbyProjectName());      //Sorting tasks by ProjectName
             displayAllTask();
         }
     }
 
-
+     /**
+     * A method to display the task details by particular task ID
+     * @param ID is the ID entered by the user for updating the task
+     */
     public void displayTaskFromList(int ID)
     {
         System.out.println("Task to be updated:");
         System.out.println("===================");
-        Task task=getTaskByID(ID);
+        Task task=taskList.get(ID-1);
         System.out.println("Task title:  "+task.getTitle());
         System.out.println("Task dueDate:"+task.getDueDate());
         System.out.println("Task project:"+task.getProjectName());
@@ -72,30 +126,48 @@ public class Task_List {
         System.out.println("=========================");
     }
 
-    public Task getTaskByID(int ID)
+    /**
+     * A method to count the number of tasks with completed status
+     * @return number of tasks with completed status
+     */
+    public int completedTaskCount()
     {
-        for(Task task:taskList){
-            int task_id=task.getTaskID();
-            if(task_id== ID)
-                return task;
-        }
-        return null;
+        return (int) taskList.stream()
+                .filter(task -> task.getStatus() == true)
+                .count();
     }
 
+
+    /**
+     * A method to count the number of tasks with incomplete status
+     * @return number of tasks with not complete status
+     */
+    public int incompleteTaskCount()
+    {
+        return (int) taskList.stream()
+                .filter(task -> task.getStatus() == false)
+                .count();
+    }
+
+
+      /**
+      * A method to update the task details by particular task ID
+      * @param ID is the ID entered by the user for updating the task
+      */
     public void updateTaskFromList(int ID)
     {
         Scanner sc=new Scanner(System.in);
 
-        Task task=getTaskByID(ID);
-        System.out.println("Press enter the following details to update thetask:");
+        Task task= taskList.get(ID-1);
+        System.out.println("Press enter the following details to update the task:");
         System.out.println("=====================================================");
         System.out.print("Task Title:");
-        String title=sc.next();
+        String title=Main.getStrInputFromUser();
         if(!(title.trim().equals("")||title==null))
             task.setTitle(title);
 
         System.out.print("Project name:");
-        String projectName=sc.next();
+        String projectName=Main.getStrInputFromUser();
         if(!(projectName.trim().equals("")||projectName==null))
             task.setProjectName(projectName);
 
@@ -109,23 +181,36 @@ public class Task_List {
         System.out.println("=========================");
     }
 
+      /**
+      * A method to delete task by particular task ID
+      * @param ID is the ID entered by the user for deleting the task
+      */
     public void deleteTaskFromList(int ID){
 
-        Task task=getTaskByID(ID);
+        Task task=taskList.get(ID-1);
         taskList.remove(task);
-        System.out.println("Tasksuccessfullydeleted.");
+        System.out.println("Task successfully deleted.");
         System.out.println("=========================");
     }
 
 
+      /**
+      * A method to mark task as complete by particular task ID
+      * @param ID is the ID entered by the user for updating the task
+      */
     public void markTaskAsDone(int ID)
     {
-        Task task=getTaskByID(ID);
+        Task task=taskList.get(ID-1);
         task.markAsDone();
         System.out.println("Task mark as done.");
         System.out.println("==================");
     }
 
+    /**
+     * This method will write the data of Tasks from ArrayList to data file on disk, i.e., tasks.obj
+     * @param fileName a string specifying the full path and extension of data file, for example,  "resources/tasks.obj"
+     * @return true if the reading operation was successful, otherwise false
+     */
     public boolean saveToFile(String fileName){
         try{
             FileOutputStream fileOutputStream=new FileOutputStream(fileName);
@@ -143,6 +228,11 @@ public class Task_List {
         }
     }
 
+     /**
+     * This method will read the data file from disk which will contain the data of previously saved tasks
+     * @param fileName a string specifying the full path and extension of data file, for example,  "resources/tasks.obj"
+     * @return true if the reading operation was successful, otherwise false
+     */    
     public boolean readFromFile(String fileName){
         boolean status=false;
 
